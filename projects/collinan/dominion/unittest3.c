2dc11d23a92ef3c75a791854e4bba478fe4e8df9
Andrew Collins <collinan@oregonstate.edu>
2019-02-10 23:38:37 -0800

    kingdomCards complete

--- projects/collinan/dominion/unittest3.c
+++ projects/collinan/dominion/unittest3.c
@@ -6,9 +6,24 @@
 #include <stdlib.h>
 
 
-//int compare(const void* a, const void* b) 
+//int* kingdomCards(int k1, int k2, int k3, int k4, int k5, int k6, int k7, int k8, int k9, int k10) 
 
 int main(){
+	
+	int expectedvalue[]={1,2,3,4,5,6,7,8,9,10};
+	int i;
+	int *temp;
+	temp= kingdomCards(1,2,3,4,5,6,7,8,9,10);
+	
+	for(i=0;i<10;i++){
+	if(temp[i]==expectedvalue[i]){
+		printf( "TEST PASSED: Values matched");
+			printf("Expected value: %d   updateCoins function return total: %d\n",expectedvalue[i],temp[i]);
+	}else{
+		printf( "TEST Failed: Values do not matched");
+		printf("Expected value: %d   updateCoins function return total: %d\n",expectedvalue[i],temp[i]);
+	}
+	}	
 	
 	
 	return 0;
